bbb5776e6ba7a9551795a07121163914816f52fb
Lucas Contreras <lucascontre95@gmail.com>
2022-05-18 12:25:12 +0200

    Stone age Problem

--- stone_age_problem/main.go
+++ stone_age_problem/main.go
@@ -1,9 +1,12 @@
+//https://codeforces.com/problemset/problem/1679/B
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const TOO_LONG = 10
@@ -16,7 +19,52 @@ func getInput() []string {
 	}
 	return input
 }
+
+func AAtoi(array []string) []int {
+	var result []int
+	for _, s := range array {
+		i, _ := strconv.Atoi(s)
+		result = append(result, i)
+	}
+	return result
+}
+
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
+
+func q1(array []int, pos int, replacement int) []int {
+	array[pos] = replacement
+	return array
+}
+
+func q2(array []int, replacement int) []int {
+	for i := range array {
+		array[i] = replacement
+	}
+	return array
+}
+
 func main() {
 	input := getInput()
-	fmt.Println(input)
+	//var output []string
+	a := AAtoi(strings.Split(input[1], " "))
+	for _, query := range input[2:] {
+		aquery := strings.Split(query, " ")
+		if aquery[0] == "1" {
+			pos, _ := strconv.Atoi(string(aquery[1]))
+			rep, _ := strconv.Atoi(string(aquery[2]))
+			q1(a, pos-1, rep)
+			fmt.Println(sum(a))
+		}
+		if query[0] == '2' {
+			rep, _ := strconv.Atoi(string(aquery[1]))
+			q2(a, rep)
+			fmt.Println(sum(a))
+		}
+	}
 }
