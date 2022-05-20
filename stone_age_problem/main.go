9c00b316721b87a03d959fdb25a492f34aa7c746
Lucas Contreras <lucascontre95@gmail.com>
2022-05-20 11:02:11 +0200

    ft2

--- stone_age_problem/main.go
+++ stone_age_problem/main.go
@@ -9,53 +9,53 @@ import (
 	"strings"
 )
 
-
-func getInput(in *bufio.Reader) [][]int {
-	var input [][]int
+func getInput(in *bufio.Reader) [][]uint {
+	var input [][]uint
 	reader := bufio.NewReader(os.Stdin)
 	line0, _ := reader.ReadString('\n')
 
 	line0 = strings.ReplaceAll(line0, "\n", "")
-	input = append(input, AAtoi(strings.Split(line0, " ")))
-	lines_count, _ := strconv.Atoi(strings.Split(line0, " ")[1])
+	input = append(input, AAtoi(line0))
+	lines_count, _ := strconv.Atoi(strings.Fields(line0)[1])
 	for i := 0; i < lines_count+1; i++ {
 		linen, _ := reader.ReadString('\n')
 		linen = strings.ReplaceAll(linen, "\n", "")
-		input = append(input, AAtoi(strings.Split(linen, " ")))
+		input = append(input, AAtoi(linen))
 	}
 	return input
 }
 
-func AAtoi(array []string) []int {
-	var result []int
+func AAtoi(st string) []uint {
+	var result []uint
+	array := strings.Fields(st)
 	for _, s := range array {
 		i, _ := strconv.Atoi(s)
-		result = append(result, i)
+		result = append(result, uint(i))
 	}
 	return result
 }
 
-func sumOnce(array []int) int {
-	result := 0
+func sumOnce(array []uint) uint {
+	result := uint(0)
 	for _, v := range array {
 		result += v
 	}
 	return result
 }
 
-func q1(array []int, pos int, replacement int) []int {
+func q1(array []uint, pos uint, replacement uint) []uint {
 	array[pos] = replacement
 	return array
 }
 
-func q2(array []int, replacement int) []int {
+func q2(array []uint, replacement uint) []uint {
 	for i := range array {
 		array[i] = replacement
 	}
 	return array
 }
 
-func contains(s []int, e int) bool {
+func contains(s []uint, e uint) bool {
 	for _, a := range s {
 		if a == e {
 			return true
@@ -64,8 +64,8 @@ func contains(s []int, e int) bool {
 	return false
 }
 
-func sumPos(a []int, positions []int) int {
-	result := 0
+func sumPos(a []uint, positions []uint) uint {
+	result := uint(0)
 	for _, p := range positions {
 		result += a[p]
 	}
@@ -79,13 +79,13 @@ func main() {
 	//q := input[0][1]
 	a := input[1]
 	lq := ""
-	i := -1
-	var positions []int
+	i := uint(0)
+	var positions []uint
 	sum := sumOnce(a)
 	for _, query := range input[2:] {
 		if query[0] == 1 {
-			pos := query[1]
-			rep := query[2]
+			pos := uint(query[1])
+			rep := uint(query[2])
 			//Actualizo el array y guardo donde actualicé
 			q1(a, pos-1, rep)
 			if !contains(positions, pos-1) {
@@ -95,7 +95,7 @@ func main() {
 			if lq == "q2" {
 				sum = sum - i + rep
 			} else if lq == "q1" {
-				sum = i*(n-len(positions)) + sumPos(a, positions)
+				sum = i*(uint(n)-uint(len(positions))) + sumPos(a, positions)
 			} else {
 				// Caso inicial
 				sum = sumOnce(a)
@@ -107,7 +107,7 @@ func main() {
 			sum = n * rep
 			i = rep
 			lq = "q2"
-			positions = []int{}
+			positions = []uint{}
 		}
 		fmt.Println(sum)
 	}
